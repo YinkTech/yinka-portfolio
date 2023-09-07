8278d30d4a485e9c62c4ae263272e7826a0d21d8
Olayinka Ayeni <layinka4dat@gmail.com>
2023-09-07 14:45:36 +0100

    remove cursor effect

--- pages/_app.js
+++ pages/_app.js
@@ -2,46 +2,18 @@ import { ChakraProvider } from "@chakra-ui/react";
 import Layout from "./../components/layouts/main";
 import theme from "./../lib/theme";
 import Fonts from "./fonts";
-import {
-  AnimatePresence,
-  motion,
-  useMotionValue,
-  useSpring,
-} from "framer-motion";
+import { AnimatePresence } from "framer-motion";
 import "./cursor.css";
-import { useEffect } from "react";
 
 const Website = ({ Component, pageProps, router }) => {
-  const cursorX = useMotionValue(-100);
-  const cursorY = useMotionValue(-100); 
-  const springConfig = { damping: 25, stiffness: 700 };
-  const cursorXSpring = useSpring(cursorX, springConfig);
-  const cursorYSpring = useSpring(cursorY, springConfig);
-
-  if (typeof window !== 'undefined') {
-    window.history.scrollRestoration = 'manual'
+  if (typeof window !== "undefined") {
+    window.history.scrollRestoration = "manual";
   }
 
-  useEffect(() => {
-    const moveCursor = (e) => {
-      cursorX.set(e.clientX - 16);
-      cursorY.set(e.clientY - 16);
-    };
-    window.addEventListener("mousemove", moveCursor);
-    return () => {
-      window.removeEventListener("mousemove", moveCursor);
-    };
-  }, [cursorX, cursorY]);
   return (
     <ChakraProvider theme={theme}>
       <Fonts />
-      <motion.div
-        className="cursor"
-        style={{
-          translateX: cursorXSpring,
-          translateY: cursorYSpring,
-        }}
-      />
+
       <Layout router={router}>
         <AnimatePresence
           mode="wait"
